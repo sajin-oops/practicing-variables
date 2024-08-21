ff7029e4c8654a3698f0e13d6b7d574cbb4a3cfc
A.K.SAJIN <108419506+sajin-oops@users.noreply.github.com>
2024-08-21 20:55:47 +0530

    Add files via upload

---
+++ variabletest.py
@@ -0,0 +1,22 @@
+# variable name should start with letter(A - Z), (a - z) or underscore(_)
+#VALID - 
+name = "Sajin"
+Name = "sajin"
+_name = "sajin"
+
+#INVALID - 
+# 1name = "sajin"
+# print(name)
+name_ = "sajin"
+print(name_)
+role = "DATA ANALYSIS"
+company = "GOOGLE"
+candidate = "SAJIN"
+
+print("congratulation " + candidate)
+print("You have been selected for a "+ role + " role at " + company)
+
+'''
+congratulation SAJIN
+You have been selected for a DATA ANALYSIS role at GOOGLE
+'''
